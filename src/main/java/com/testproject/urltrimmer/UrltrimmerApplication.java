b5150a93cec4f73cd4a55f0a274f068145cf2776
Alex <clearwinstonsigi@gmail.com>
2022-01-20 19:11:11 +0300

    Initial commit and add db

---
+++ src/main/java/com/testproject/urltrimmer/UrltrimmerApplication.java
@@ -0,0 +1,14 @@
+package com.testproject.urltrimmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrltrimmerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrltrimmerApplication.class, args);
+        
+    }
+
+}
---
+++ src/test/java/com/testproject/urltrimmer/UrltrimmerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testproject.urltrimmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrltrimmerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
